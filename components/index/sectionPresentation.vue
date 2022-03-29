432a12789c48c618a0f926577ce29a1cdf97c416
y4wee <quentin.jambert@gmail.com>
2022-03-29 20:11:34 +0200

    section height

--- components/index/sectionPresentation.vue
+++ components/index/sectionPresentation.vue
@@ -127,6 +127,7 @@ $colorYellow: #c9853c;
     flex-direction: column;
     align-items: center;
     width: 100%;
+    min-height: 100vh;
     overflow-x: hidden;
     &Head {
       display: flex;
