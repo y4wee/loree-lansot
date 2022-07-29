e1606cd10350b3947cf576e2c2ccb224b5d39987
y4wee <quentin.jambert@gmail.com>
2022-07-29 12:54:20 +0200

    description ptit dej

--- components/index/sectionDelice.vue
+++ components/index/sectionDelice.vue
@@ -1,68 +1,57 @@
 <template>
-    <div class="delice">
-        <HalfTitle 
-        :text="'Nos Delices'"
-        />
-        <section class="deliceDejeuner">
-            <CarteText 
-            class="deliceDejeunerCarte"
-            :carte="carteDejeuner"
-            />
-            <MosaiqueDejeuner 
-            class="deliceDejeunerMosaique"
-            />
-        </section>
+  <div class="delice">
+    <HalfTitle :text="'Nos Delices'" />
+    <section class="deliceDejeuner">
+      <CarteText class="deliceDejeunerCarte" :carte="carteDejeuner" />
+      <MosaiqueDejeuner class="deliceDejeunerMosaique" />
+    </section>
 
-        <section class="deliceRepas">
-            <CarteText 
-            class="deliceRepasCarte"
-            :carte="carteRepas"
-            />
-            <div class="deliceRepasPhoto">
-                <img class="deliceRepasPhotoImage" src="~/assets/dejeuner/repas.jpg" alt="photo plateau repas de l'orée de lansot">
-            </div>
-        </section>
-    </div>
+    <section class="deliceRepas">
+      <CarteText class="deliceRepasCarte" :carte="carteRepas" />
+      <div class="deliceRepasPhoto">
+        <img
+          class="deliceRepasPhotoImage"
+          src="~/assets/dejeuner/repas.jpg"
+          alt="photo plateau repas de l'orée de lansot"
+        />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import MosaiqueDejeuner from './mosaiqueDejeuner.vue';
-import CarteText from '../carteText.vue';
-import HalfTitle from '../halfTitle.vue';
+import MosaiqueDejeuner from "./mosaiqueDejeuner.vue";
+import CarteText from "../carteText.vue";
+import HalfTitle from "../halfTitle.vue";
 
 export default {
-    name: "Delice",
-    data() {
-        return {
-            carteDejeuner: {
-                text: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Sequi quod optio sit? Dolor ipsum minus hic. 
-                        Corporis laborum voluptate, possimus odit culpa, 
-                        ut temporibus earum ratione hic ipsam sed porro amet quaerat! 
-                        Ex ipsa inventore in repudiandae optio neque ullam eius iure!
-                        Iste, magni est assumenda numquam recusandae doloremque laudantium.`,
-                lottie: "https://assets3.lottiefiles.com/packages/lf20_gbypohim.json"
-            },
-            carteRepas: {
-                text: ` Pour la table table d'hôtes, nous proposons un plateau repas à 15 euros par personne, 
+  name: "Delice",
+  data() {
+    return {
+      carteDejeuner: {
+        text: ` 
+                        Dégustez dès le réveil les bons produits que nous vous proposons. 
+                        Au menu, des patisseries et du pain fait-maison, confitures diverses et variées, 
+                        viennoiseries locales, à déguster autour de variétés de thés, cafés, chocolat et jus d'orange.`,
+        lottie: "https://assets3.lottiefiles.com/packages/lf20_gbypohim.json",
+      },
+      carteRepas: {
+        text: ` Pour la table table d'hôtes, nous proposons un plateau repas à 15 euros par personne, 
                         celui-ci se compose d'une salade verte, œuf poché, jambon, pâté, 
                         fromage et un verre de vin. (merci de nous le préciser lors de votre réservation).`,
-                lottie: "https://assets10.lottiefiles.com/packages/lf20_2cmperww.json"
-            },
-        }
-    },
-    mounted() {
-    },
-    methods: {},
-    components: { MosaiqueDejeuner, CarteText, HalfTitle }
-}
-
+        lottie: "https://assets10.lottiefiles.com/packages/lf20_2cmperww.json",
+      },
+    };
+  },
+  mounted() {},
+  methods: {},
+  components: { MosaiqueDejeuner, CarteText, HalfTitle },
+};
 </script>
 
 <style scoped lang="scss">
-
 $colorBrown: #291315;
-$colorGreen: #8FBF21; //good
+$colorGreen: #8fbf21; //good
 $colorWhite: #f7f6f8; //good
 $colorBlue: #131b1e;
 $colorGray: #d1cfcf;
@@ -71,93 +60,92 @@ $colorOrange: #e25827;
 $colorYellow: #c4721c;
 
 .delice {
-    z-index: 1;
-    position: relative;
+  z-index: 1;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+  overflow-x: hidden;
+  background: $colorWhite;
+  border-top: solid 10px $colorBlue;
+  border-bottom: solid 10px $colorWhite;
+  user-select: none;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    width: 50%;
+    background: $colorBlue;
+  }
+  &Dejeuner {
+    z-index: 2;
+    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-evenly;
+    &Mosaique {
+      margin: 50px 0;
+    }
+  }
+  &Repas {
+    z-index: 2;
     width: 100%;
-    overflow-x: hidden;
-    background: $colorWhite;
-    border-top: solid 10px $colorBlue;
-    border-bottom: solid 10px $colorWhite;
-    user-select: none;
-    &::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        width: 50%;
-        background: $colorBlue;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    &Photo {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: calc(100% - 20px);
+      height: 300px;
+      margin: 50px 10px;
+      box-shadow: 2px 2px 5px 0 rgba($color: #000000, $alpha: 0.7);
+      overflow: hidden;
+      &Image {
+        object-fit: cover;
+        width: 101%;
+        height: 101%;
+      }
     }
-    &Dejeuner {
-        z-index: 2;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        &Mosaique {
-            margin: 50px 0;
-        }
-    }
-    &Repas {
-        z-index: 2;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        &Photo {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: calc(100% - 20px);
-            height: 300px;
-            margin: 50px 10px;
-            box-shadow: 2px 2px 5px 0 rgba($color: #000000, $alpha: 0.7);
-            overflow: hidden;
-            &Image {
-                object-fit: cover;
-                width: 101%;
-                height: 101%;
-            }
-        }
-    }
+  }
 }
 @media all and (min-width: 750px) {
-    .delice {
-        &Repas {
-            &Photo {
-                height: auto;
-                max-height: 600px;
-            }
-        }
+  .delice {
+    &Repas {
+      &Photo {
+        height: auto;
+        max-height: 600px;
+      }
     }
+  }
 }
 @media all and (min-width: 1025px) {
-    .delice {
-        &Dejeuner {
-            &Carte {
-                align-self: flex-start;
-                margin: 0 5%;
-            }
-            &Mosaique {
-                margin: 50px 5%;
-            }
-        }
-        &Repas {
-            &Carte {
-                align-self: flex-start;
-                margin: 0 5%;
-            }
-            &Photo {
-                align-self: flex-end;
-                width: 75%;
-                margin: 50px 5%;
-            }
-        }
+  .delice {
+    &Dejeuner {
+      &Carte {
+        align-self: flex-start;
+        margin: 0 5%;
+      }
+      &Mosaique {
+        margin: 50px 5%;
+      }
     }
+    &Repas {
+      &Carte {
+        align-self: flex-start;
+        margin: 0 5%;
+      }
+      &Photo {
+        align-self: flex-end;
+        width: 75%;
+        margin: 50px 5%;
+      }
+    }
+  }
 }
-
 </style>
